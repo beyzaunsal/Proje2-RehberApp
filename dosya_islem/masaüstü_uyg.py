007bb08798a955eab794aa5cae25de398755dde4
beyzaunsal <unsalbeyza678@gmail.com>
2024-09-02 03:09:51 +0300

    güncel

--- dosya_islem/masaüstü_uyg.py
+++ dosya_islem/masaüstü_uyg.py
@@ -1,13 +1,68 @@
+# from PyQt6.QtWidgets import *
+
+# app = QApplication([])
+# label = QLabel("Merhaba!")
+# label.show()
+
+# app.exec()
+
+# xxx = QApplication([])
+# label = label = QLabel("Merhaba2!")
+# label.show()
+# app.exec()
+
+# import sys
+# from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+# app = QApplication(sys.argv) # type: ignore
+
+# x = QWidget()
+
+# x.setWindowTitle("Deneme")
+# x.resize(300,100)
+# # x.setFixedSize(100, 100)
+
+# x.show()
+
+
+# window1 = QPushButton("tıkla")
+# x.setWindowTitle("Deneme22")
+# x.resize(300,100)
+# # x.setFixedSize(100, 100)
+
+
+# window1.show()
+# aa = QLabel("MERHABA")
+# aa.show()
+
+# app.exec()
+
 from PyQt6.QtWidgets import *
 
-app = QApplication([])
-label = QLabel("Merhaba!")
-label.show()
+class CeviriPenceresi(QMainWindow):
 
-app.exec()
+    def __init__(self,baslik,g=0,y=0):
+        super().__init__() 
+        super().setWindowTitle(baslik)
+        if g!=0: and y!=0: self.resize(g,y)
 
-xxx = QApplication([])
-label = label = QLabel("Merhaba2!")
-label.show()
-app.exec()
+        içerik = QVBoxLayout()
+
+        içerik.addWidget(QLabel("Çevrilecek :"))
+        içerik.addWidget(QLineEdit(""))
+        içerik.addWidget(QPushButton("Çevir"))
+        içerik.addWidget(QLabel("Sonuç : "))
+
+        araclar = QWidget()
+        araclar.setLayout(içerik)
+        self.setCentralWidget(araclar)
+
+uygulama = QApplication([])
+
+pencere = CeviriPenceresi("Pencere1")
+pencere2 = CeviriPenceresi("Pencere2")
+pencere.show()
+pencere2.show()
+         
+uygulama.exec()
 
